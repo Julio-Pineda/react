dc4a031eafa3fd0e82c252d6b4cd0c53bfff7265
Julio Pineda <pjulio@unicauca.edu.co>
2023-08-18 15:24:32 -0500

    Unir componentes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { TodoButton } from './TodoButton';
-
+import React from 'react'; //Se debe usar este, para que fragment funcione
 import './App.css';
 
 
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Curso de react', completed: false},
+  {text: 'Llorar con la llorona', completed: false},
+  {text: 'Pelar el cobre', completed: false},
+  {text: 'Tomar la cena', completed: false},
+];
+
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
 
-      <TodoCounter/>
+    <React.Fragment>
+      <TodoCounter completed={10} total={12}/>
       <TodoSearch/>
 
       <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
-       <TodoButton/>
-    </div>
+       <TodoButton />
+    </React.Fragment>
+    // </div>
   );
 }
 
